aa56a7b32eb40ee2e8372d312d5061eee90fe5a6
Wojciech Lewandowski <lw51032@zut.edu.pl>
2024-06-06 09:20:47 +0200

    Change primary display to work in portrait mode.
    Primary display is now portrait and has portrait
    UI design. Screen density is set to 120.
    
    vendor.prop - device configuration file.
    
    Signed-off-by: Wojciech Lewandowski <lw51032@zut.edu.pl>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,2 +1,8 @@
 # include BoardConfig.mk
 include device/brcm/rpi4/BoardConfig.mk
+
+# Override brcm/rpi4 properties
+TARGET_VENDOR_PROP := device/ipz-automotive/zoobr/vendor.prop
+
+# Display
+TARGET_SCREEN_DENSITY := 120
--- aosp_zoobr_car.mk
+++ aosp_zoobr_car.mk
@@ -1,5 +1,28 @@
+# Add boot animation
+PRODUCT_COPY_FILES += \
+    packages/services/Car/car_product/car_ui_portrait/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
 # Inherit aosp_rpi4_car
 include device/brcm/rpi4/aosp_rpi4_car.mk
 
 PRODUCT_NAME := aosp_zoobr_car
 PRODUCT_DEVICE := zoobr
+
+# set portrait mode
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.primary_display_orientation=ORIENTATION_270
+
+# Exclude AAE Car System UI
+DO_NOT_INCLUDE_AAE_CAR_SYSTEM_UI := true
+
+# Exclude Car UI Reference Design
+DO_NOT_INCLUDE_CAR_UI_REFERENCE_DESIGN := true
+
+$(call inherit-product, packages/services/Car/car_product/car_ui_portrait/apps/car_ui_portrait_apps.mk)
+$(call inherit-product, packages/services/Car/car_product/car_ui_portrait/rro/car_ui_portrait_rro.mk)
+
+PRODUCT_COPY_FILES += \
+    packages/services/Car/car_product/car_ui_portrait/car_ui_portrait_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/car_ui_portrait_hardware.xml
+
+PRODUCT_COPY_FILES += \
+    packages/services/Car/car_product/car_ui_portrait/car_ui_portrait_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/car_ui_portrait_hardware.xml
